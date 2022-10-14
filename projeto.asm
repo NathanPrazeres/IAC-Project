d1dba596a6cff7d23d56ec447af5b6de997c2f6a
NathanPrazeres <90866687+NathanPrazeres@users.noreply.github.com>
2022-10-14 19:38:36 +0100

    Update projeto.asm

--- projeto.asm
+++ projeto.asm
@@ -12,16 +12,21 @@
 DISPLAY					EQU	0A000H
 TEC_LIN					EQU	0C000H		; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL					EQU	0E000H		; endereço das colunas do teclado (periférico PIN)
-MAX_TECLADO				EQU	16			; linha a testar (4ª linha, 1000b)
+MAX_TECLADO				EQU	16
 MASCARA					EQU	0FH			; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
 ; Teclas para controlo do jogo
-TECLA_ESQUERDA			EQU	0			
+TECLA_ESQUERDA			EQU	0
+TECLA_DISPARO			EQU 1			
 TECLA_DIREITA			EQU	2			
 TECLA_MOVE_INIMIGO		EQU	3			
 TECLA_DIMINUI			EQU	4			 
-TECLA_AUMENTA			EQU	5			
+TECLA_AUMENTA			EQU	5
+TECLA_COMECA			EQU 12
+TECLA_PAUSA				EQU 13
+TECLA_TERMINAR			EQU 14
 
+; Comandos Media-Center
 DEFINE_LINHA			EQU	600AH		; endereço do comando para definir a linha
 DEFINE_COLUNA			EQU	600CH		; endereço do comando para definir a coluna
 DEFINE_PIXEL			EQU	6012H		; endereço do comando para escrever um pixel
@@ -33,14 +38,21 @@ REPRODUZ_SOM			EQU	605AH		; endereço do comando para reproduzir o som seleciona
 
 CENARIO_JOGO			EQU	0
 
+N_LINHAS				EQU	32
+N_COLUNAS				EQU	64
+
 LINHA_PLAYER			EQU	27			; linha do boneco (a meio do ecrã))
 COLUNA_PLAYER			EQU	30			; coluna do boneco (a meio do ecrã)
 
-LINHA_INIMIGO			EQU	0			; linha do inimigo
-COLUNA_INIMIGO			EQU	10			; coluna do inimigo
+LINHA_METEORO			EQU	0			; linha do inimigo
+COLUNA_METEORO			EQU	10			; coluna do inimigo
+
+MUDA_METEORO			EQU	3			; numero de linhas a andar até mudar o meteoro (fazer)
 
 LARGURA					EQU	5			; largura do boneco
 ALTURA					EQU	5			; altura do boneco
+
+N_METEOROS				EQU 4			; numero de bonecos a usar no jogo
 
 DIMENSAO_METEORO_1		EQU 1			; dimensão do meteoro distante (1 x 1)
 
@@ -67,7 +79,6 @@ VERMELHO_PRETO			EQU	0F300H
 VERMELHO_ESCURO			EQU	0F800H		
 VERMELHO_MEDIO			EQU	0FC00H
 VERMELHO				EQU 0FF00H
-DOURADO					EQU	0FFC0H
 
 ;Cores inimigo:
 ROXO_PRETO				EQU 0F303H
@@ -99,49 +110,115 @@ PRETO					EQU 0F000H
 ; >> R11 e R12 não podem ser usados <<
 ; *********************************************************************************
 
-	PLACE	1000H
+	PLACE	2000H
 pilha:
-	STACK	200H			; espaço reservado para a pilha 
-							; (400H bytes, pois são 200H words)
-SP_inicial:					; este é o endereço (1400H) com que o SP deve ser 
-							; inicializado. O 1.º end. de retorno será 
-							; armazenado em 13FEH (1400H-2)
+	STACK	100H
+	
+SP_inicial:	
+	STACK	100H				
 
-linha_boneco:				; linha em que cada boneco está (inicializada com a linha inicial)
-	WORD		LINHA_PLAYER
-	WORD		LINHA_INIMIGO
+SP_teclado:
+	STACK	100H
+
+tab_int:
+	WORD rot_int_0			; rotina de atendimento da interrup��o 0
+	WORD rot_int_1			; rotina de atendimento da interrup��o 1
+	WORD rot_int_2			; rotina de atendimento da interrup��o 2
+	WORD rot_int_3			; rotina de atendimento da interrup��o 3
+
+evento_int:
+	WORD 0				; se 1, indica que a interrup��o 0 ocorreu
+	WORD 0				; se 1, indica que a interrup��o 1 ocorreu
+	WORD 0				; se 1, indica que a interrup��o 2 ocorreu
+	WORD 0				; se 1, indica que a interrup��o 3 ocorreu
                               
-coluna_boneco:				; coluna em que cada boneco está (inicializada com a coluna inicial)
+linha_barra:
+	WORD 0				; linha em que a 1ª barra está
+	WORD 0				; linha em que a 2ª barra está
+	WORD 0				; linha em que a 3ª barra está
+	WORD 0				; linha em que a 4ª barra está
+
+linhas:				; linha em que cada boneco está (inicializada com a linha inicial)
+	WORD		LINHA_PLAYER
+	WORD		LINHA_METEORO
+	WORD		LINHA_METEORO
+	WORD		LINHA_METEORO
+	WORD		LINHA_METEORO
+                              
+colunas:				; coluna em que cada boneco está (inicializada com a coluna inicial)
 	WORD		COLUNA_PLAYER
-	WORD		COLUNA_INIMIGO
-							
+	WORD		COLUNA_METEORO
+	WORD		COLUNA_METEORO
+	WORD		COLUNA_METEORO
+	WORD		COLUNA_METEORO
+
+estado_bom:
+	WORD		0
+	WORD		DEF_BOM_LONGE
+	WORD		DEF_BOM_PEQUENO
+	WORD		DEF_BOM_MEDIO
+	WORD		DEF_BOM_4
+	WORD		DEF_BOM_GRANDE
+
+estado_inimigo:
+	WORD		DEF_INIMIGO_LONGE
+	WORD 		DEF_INIMIGO_PEQUENO
+	WORD		DEF_INIMIGO_MEDIO
+	WORD		DEF_INIMIGO_4
+	WORD		DEF_INIMIGO_GRANDE
+
+contador:
+	WORD		100H
+
+sub_display:					; subtrações necessárias a fazer ao display para andar de 5 em 5
+	WORD		2
+	WORD		107
+	WORD		5
+	WORD		11
+
+meteoros:
+	WORD		1
+	WORD		1
+	WORD		2
+	WORD		1
+
+tecla_carregada:
+	LOCK		-1
+
+; Modelo do jogador:
+
 DEF_PLAYER:					; tabela que define o boneco (altura, largura, pixels)
 	WORD		LARGURA
 	WORD		ALTURA
 	WORD		0, 0, VERMELHO, 0, 0								
 	WORD		0, 0, VERMELHO_MEDIO, 0, 0								; display das cores do jogador
 	WORD		VERMELHO_MEDIO, 0, VERMELHO_ESCURO, 0, VERMELHO_MEDIO
-	WORD		VERMELHO_PRETO, DOURADO, VERMELHO_PRETO, DOURADO, VERMELHO_PRETO
+	WORD		VERMELHO_PRETO, VERMELHO_PRETO, VERMELHO_PRETO, VERMELHO_PRETO, VERMELHO_PRETO
 	WORD		0, PRETO, 0, PRETO, 0
+
 
 ; Modelo dos meteoros inimigos:
 
 DEF_INIMIGO_LONGE:			; tabela que define o inimigo longe (dimensão, pixels)
 	WORD 				DIMENSAO_METEORO_1
+	WORD 				DIMENSAO_METEORO_1
 	WORD				ROXO_TRANS
 
 DEF_INIMIGO_PEQUENO:		; tabela que define o inimigo pequeno (dimensão, pixels)
+	WORD				DIMENSAO_METEORO_2
 	WORD				DIMENSAO_METEORO_2
 	WORD				ROXO_BACO, ROXO_BACO
 	WORD				ROXO_BACO, ROXO_BACO
 
 DEF_INIMIGO_MEDIO:			; tabela que define o inimigo médio (dimensão, pixels)
 	WORD				DIMENSAO_METEORO_3
+	WORD				DIMENSAO_METEORO_3
 	WORD				ROXO_PRETO, ROXO_PRETO, ROXO_PRETO
 	WORD				ROXO_ESCURO, PRETO, ROXO_ESCURO
 	WORD 				ROXO_MEDIO, ROXO_MEDIO, ROXO_MEDIO
 
 DEF_INIMIGO_4:				; tabela que define o inimigo na penúltima fase (dimensão, pixels)
+	WORD				DIMENSAO_METEORO_4
 	WORD				DIMENSAO_METEORO_4
 	WORD				ROXO_PRETO, ROXO_PRETO, ROXO_PRETO, ROXO_PRETO
 	WORD				ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO
@@ -156,6 +233,7 @@ DEF_INIMIGO_GRANDE:			; tabela que define o inimigo final (altura, largura, pixe
 	WORD				ROXO_ESCURO, ROXO_MEDIO, BRANCO, ROXO_MEDIO, ROXO_ESCURO
 	WORD				ROXO_MEDIO, ROXO_FORTE, ROXO_FORTE, ROXO_FORTE, ROXO_MEDIO
 	WORD				0, ROXO_BRANCO, ROXO_BRANCO, ROXO_BRANCO, 0
+
 
 ; Modelos dos meteoros bons:
 
@@ -174,7 +252,7 @@ DEF_BOM_MEDIO:				; tabela que define o meteoro bom médio (dimensão, pixels)
 	WORD				AMARELO_BRANCO, BRANCO, AMARELO_BRANCO
 	WORD				AMARELO_FORTE, AMARELO_BRANCO, AMARELO_FORTE
 
-DEF_BOM_MEDIO_4:			; tabela que define o meteoro bom na penúltima fase (dimensão, pixels)
+DEF_BOM_4:			; tabela que define o meteoro bom na penúltima fase (dimensão, pixels)
 	WORD				DIMENSAO_METEORO_4
 	WORD				0, AMARELO_FORTE, AMARELO_FORTE, 0
 	WORD				AMARELO_FORTE, AMARELO_BRANCO, AMARELO_BRANCO, AMARELO_FORTE
@@ -192,26 +270,40 @@ DEF_BOM_GRANDE:				; tabela que define o meteoro bom final (altur, largura, pixe
 	WORD				0, AMARELO_FORTE, AMARELO_FORTE, AMARELO_FORTE, 0
 
 
+
+
 ; *********************************************************************************
 ; * Código
 ; *********************************************************************************
 	PLACE	0								; o código tem de começar em 0000H
 inicio:
 	MOV		SP, SP_inicial					; inicializa SP para a palavra a seguir à última da pilha
+
+	MOV		BTE, tab_int
                             
     MOV		[APAGA_AVISO], R1				; apaga o aviso de nenhum cenário selecionado
 	MOV		[APAGA_ECRA], R1				; apaga o ecrã
 	MOV		R1, CENARIO_JOGO				; cenário do jogo
     MOV		[SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
 	MOV		R1, 0
-	MOV		[DISPLAY], R1					; mete no display o cenário
-	MOV		R11, 0
+	MOV		R1, [contador]
+	MOV		[DISPLAY], 	R1					; mete no display o cenário
+	MOV		R11, N_METEOROS					; número de meteoros em jogo
 
+	;EI2										; permite interrupções 2
+	;EI0										; permite interrupções 0
+	;EI1										; permite interrupções 1
+	;EI3										; permite interrupções 3
+	;EI										; permite interrupções (geral)
+
+; ciclo:
+
+	CALL	teclado
 
 bonecos_iniciais:
-	MOV		R1, LINHA_INIMIGO				; linha do inimigo
-	MOV		R2, COLUNA_INIMIGO				; coluna do inimigo
-	MOV		R4, DEF_BOM_GRANDE				; endereço da tabela que define o inimigo
+	MOV		R1, LINHA_METEORO				; linha do inimigo
+	MOV		R2, COLUNA_METEORO				; coluna do inimigo
+	MOV		R4, DEF_INIMIGO_MEDIO			; endereço da tabela que define o inimigo
 	CALL	desenha_boneco					; desenha o inimigo
 
     MOV		R1, LINHA_PLAYER				; linha do boneco
@@ -220,11 +312,9 @@ bonecos_iniciais:
 
 mostra_boneco:
 	CALL	desenha_boneco					; desenha o boneco a partir da tabela
-
-espera_tecla:								; neste ciclo espera-se até uma tecla ser premida
-	CALL	teclado							; leitura às teclas
-	CMP		R0, -1			
-	JZ		espera_tecla					; continua a ler até uma tecla ser selecionada
+	
+espera_tecla:
+	MOV		R0, [tecla_carregada]
 
 testa_esquerda:
 	MOV		R7, TECLA_ESQUERDA
@@ -246,22 +336,18 @@ diminui_display:							; vai diminuir o numero no display de sete segmentos
 	MOV		R7, TECLA_DIMINUI
 	CMP		R0, R7
 	JNZ		aumenta_display
-	SUB		R11, 5
-	MOV		[DISPLAY], R11					; subtrair 5 ao valor no display
-	CALL	pressionada
+	CALL	diminui
 	JMP		espera_tecla
 
 aumenta_display:							; vai aumentar o numero no display de sete segmentos
 	MOV		R7, TECLA_AUMENTA
 	CMP		R0, R7
-	JNZ		move_inimigo
-	ADD		R11, 5
-	MOV		[DISPLAY], R11					; adiciona 5 ao valor no display
-	CALL	pressionada
+	JNZ		move_meteoro
+	CALL	aumenta
 	JMP		espera_tecla
 
 
-move_inimigo:								; move o meteoro inimigo para baixo
+move_meteoro:								; move o meteoro inimigo para baixo
 	MOV		R7, TECLA_MOVE_INIMIGO
 	CMP		R0, R7
 	JNZ		espera_tecla
@@ -284,6 +370,42 @@ coluna_seguinte:
 	JMP		mostra_boneco					; vai desenhar o boneco de novo
 
 
+diminui:
+	PUSH 	R1
+	PUSH	R2
+	PUSH	R3
+	PUSH	R4
+	MOV		R3, sub_display
+	MOV		R4, [R3]
+	MOV		R2, [R3 + R4]
+	ADD		R4, 2
+	MOV		R1, [contador]
+	SUB		R1, R2
+	ADD		R3, 2
+	MOV		R2, [R3]
+	MOV		[DISPLAY], R1	
+	MOV		[contador], R1
+	MOV		[R3], R4
+	CALL	pressionada
+	POP		R4
+	POP		R3
+	POP		R2
+	POP 	R1
+	RET
+
+
+aumenta:
+	PUSH 	R1
+	MOV		R1, [contador]
+	ADD		R1, 5
+	MOV		[DISPLAY], R1					; adiciona 5 ao valor no display
+	MOV		[contador], R1
+	CALL	pressionada
+	POP		R1
+	RET
+
+
+
 ; **********************************************************************
 ; DESENHA_BONECO - Desenha um boneco na linha e coluna indicadas
 ;			    com a forma e cor definidas na tabela indicada.
@@ -300,7 +422,9 @@ desenha_boneco:
 	PUSH	R4
 	PUSH	R5
 	PUSH	R6
-	MOV		R5, [R4]						; obtém a largura do boneco
+	PUSH	R7
+	MOV		R5,	[R4] 						; obtém a largura do boneco
+	MOV		R7, [R4]
 	ADD		R4, 2							; passa para a próxima word 
 	MOV		R6, [R4]						; altura do boneco
 	ADD		R4, 2							; passa para a próxima word 
@@ -312,11 +436,12 @@ desenha_pixels:       						; desenha os pixels do boneco a partir da tabela
     ADD		R2, 1               			; próxima coluna
     SUB		R5, 1							; menos uma coluna para tratar
     JNZ		desenha_pixels      			; continua até percorrer toda a largura do objeto
-	SUB		R2, LARGURA						; dá reset ao contador a 0
-	ADD		R5, LARGURA						; adiciona de volta a largura das colunas que são precisas tratar
+	SUB		R2, R7						; dá reset ao contador a 0
+	ADD		R5, R7						; adiciona de volta a largura das colunas que são precisas tratar
 	ADD		R1, 1							; próxima linha
 	SUB		R6, 1							; menos uma linha para tratar
 	JNZ		desenha_pixels					; continua a percorrer a largura até não haverem mais linhas para tratar
+	POP		R7
 	POP		R6
 	POP		R5
 	POP		R4
@@ -358,6 +483,8 @@ apaga_pixels:       						; desenha os pixels do boneco a partir da tabela
 	ADD		R1, 1							; mais uma linha apagada
 	SUB		R6, 1							; menos uma linha para tratar
 	JNZ		apaga_pixels					; continua a percorrer a figura em altura até apagar tudo
+	
+acaba_apagar:
 	POP		R6
 	POP		R5
 	POP		R4
@@ -385,15 +512,15 @@ escreve_pixel:
 ; Argumentos:   R11 - valor que define o atraso
 ;
 ; **********************************************************************
-atraso:
-	PUSH	R11
-	MOV		R11, ATRASO
+;atraso:
+	;PUSH	R11
+	;MOV		R11, ATRASO
 
-ciclo_atraso:
-	SUB		R11, 1
-	JNZ		ciclo_atraso
-	POP		R11
-	RET
+;ciclo_atraso:
+;	SUB		R11, 1
+;	JNZ		ciclo_atraso
+;	POP		R11
+;	RET
 
 ; **********************************************************************
 ; TESTA_LIMITES - Testa se o boneco chegou aos limites do ecrã e nesse caso
@@ -434,26 +561,30 @@ sai_testa_limites:
 	POP		R5
 	RET
 
-
-
 move_boneco_baixo:
 	PUSH	R1
 	PUSH	R2
 	PUSH	R3
 	PUSH	R4
 	PUSH	R5
-	MOV		R3, linha_boneco				; obtem endereço de tabela de posicoes (linha) dos bonecos
-	MOV		R5, coluna_boneco				; obtem endereço de tabela de posicoes (coluna) dos bonecos
-	MOV		R1, [R3+2]						; obtem a linha do boneco inimigo
-	MOV		R2, [R5+2]						; obtem a coluna do boneco inimigo
-	MOV		R4, DEF_BOM_GRANDE				; obtem tabela que define o boneco
+	PUSH	R6
+	MOV		R3, linhas						; obtem endereço de tabela de posicoes (linha) dos bonecos
+	MOV		R5, colunas						; obtem endereço de tabela de posicoes (coluna) dos bonecos
+	MOV		R6, [R3]						; obtem a linha do jogador
+	MOV		R1, [R3+2]						; obtem a linha do meteoro
+	MOV		R2, [R5+2]						; obtem a coluna do meteoro
+	MOV		R4, DEF_INIMIGO_MEDIO			; obtem tabela que define o boneco
 	CALL	apaga_boneco		
 	ADD		R1, 1							; obtem próxima linha
+	; COMPARAR COM A LINHA E COLUNA DO JOGADOR  PARA VER SE HÁ COLISÃO
 	MOV		[R3+2], R1						; regista proxima linha
 	CALL	desenha_boneco					; desenha boneco na nova posicao
 	MOV		R1, 0
 	MOV		[SELECIONA_SOM], R1 			; obtem som do boneco a movimentar-se
-	MOV		[REPRODUZ_SOM], R1	
+	MOV		[REPRODUZ_SOM], R1
+
+acaba_mover_baixo:		
+	POP 	R6		
 	POP		R5
 	POP		R4
 	POP		R3
@@ -467,47 +598,47 @@ move_boneco_baixo:
 ;
 ; Retorna: 	R0 - valor lido das colunas do teclado (1, 2, 4, ou 8)	
 ; **********************************************************************
-teclado:
-	PUSH	R2
-	PUSH	R3
-	PUSH	R5
-	PUSH 	R6
-	PUSH 	R7
-	MOV	 	R2, TEC_LIN   					; endereço do periférico das linhas
-	MOV	 	R3, TEC_COL   					; endereço do periférico das colunas
-	MOV	 	R5, MASCARA   					; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-	MOV		R6, MAX_TECLADO
+;teclado:
+;	PUSH	R2
+;	PUSH	R3
+;	PUSH	R5
+;	PUSH 	R6
+;	PUSH 	R7
+;	MOV	 	R2, TEC_LIN   					; endereço do periférico das linhas
+;	MOV	 	R3, TEC_COL   					; endereço do periférico das colunas
+;	MOV	 	R5, MASCARA   					; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+;	MOV		R6, MAX_TECLADO
+;
+;
+;loop_teclado:
+;	SHR		R6, 1
+;	JZ		nenhuma							; se chegar ao fim do loop e nenhuma tecla for premida
+;
+;	MOVB 	[R2], R6      					; a linha a ler passa a ser a do registo R6
+;	MOVB 	R0, [R3]      					; ler do periférico de entrada (colunas)
+;	
+;	AND		R0, R5        					; elimina bits para além dos bits 0-3
+;	JNZ		fim_teclado						; se alguma tecla for premida quebra o loop
+;	
+;	JMP		loop_teclado
+;
+;nenhuma:
+;	MOV		R0, -1							; quando nenhuma tecla está selecionada mete R0 a -1
+;	JMP		skip
+;
+;fim_teclado:
+;	CALL	converte_tec_hex
+;
+;skip:
+;	POP		R6
+;	POP		R7
+;	POP		R5
+;	POP		R3
+;	POP		R2
+;	RET
 
 
-loop_teclado:
-	SHR		R6, 1
-	JZ		nenhuma							; se chegar ao fim do loop e nenhuma tecla for premida
-
-	MOVB 	[R2], R6      					; a linha a ler passa a ser a do registo R6
-	MOVB 	R0, [R3]      					; ler do periférico de entrada (colunas)
-	
-	AND		R0, R5        					; elimina bits para além dos bits 0-3
-	JNZ		fim_teclado						; se alguma tecla for premida quebra o loop
-	
-	JMP		loop_teclado
-
-nenhuma:
-	MOV		R0, -1							; quando nenhuma tecla está selecionada mete R0 a -1
-	JMP		skip
-
-fim_teclado:
-	CALL	converte_tec_hexa
-
-skip:
-	POP		R7
-	POP		R6
-	POP		R5
-	POP		R3
-	POP		R2
-	RET
-
-
-converte_tec_hexa:
+converte_tec_hex:
 	PUSH	R6
 	PUSH	R7
 
@@ -555,3 +686,119 @@ loop_pressionada:
 	POP		R3
 	POP		R0
 	RET
+
+
+
+
+
+; **********************************************************************
+; ROT_INT_0 - 	Rotina de atendimento da interrup��o 0
+;			Assinala o evento na componente 0 da vari�vel evento_int
+; **********************************************************************
+rot_int_0:
+	PUSH R0
+	PUSH R1
+	MOV  R0, evento_int
+	MOV  R1, 1			; assinala que houve uma interrup��o 0
+	MOV  [R0], R1			; na componente 0 da vari�vel evento_int
+	POP  R1
+	POP  R0
+	RFE
+
+; **********************************************************************
+; ROT_INT_1 - 	Rotina de atendimento da interrup��o 1
+;			Assinala o evento na componente 1 da vari�vel evento_int
+; **********************************************************************
+rot_int_1:
+	PUSH R0
+	PUSH R1
+	MOV  R0, evento_int
+	MOV  R1, 1			; assinala que houve uma interrup��o 0
+	MOV  [R0+2], R1		; na componente 1 da vari�vel evento_int
+						; Usa-se 2 porque cada word tem 2 bytes
+	POP  R1
+	POP  R0
+	RFE
+
+; **********************************************************************
+; ROT_INT_2 -	Rotina de atendimento da interrup��o 2
+;			Assinala o evento na componente 2 da vari�vel evento_int
+; **********************************************************************
+rot_int_2:
+	PUSH R0
+	PUSH R1
+	MOV  R0, evento_int
+	MOV  R1, 1			; assinala que houve uma interrup��o 0
+	MOV  [R0+4], R1		; na componente 2 da vari�vel evento_int
+						; Usa-se 4 porque cada word tem 2 bytes
+	POP  R1
+	POP  R0
+	RFE
+
+; **********************************************************************
+; ROT_INT_3 -	Rotina de atendimento da interrup��o 3
+;			Assinala o evento na componente 3 da vari�vel evento_int
+; **********************************************************************
+rot_int_3:
+	PUSH R0
+	PUSH R1
+	MOV  R0, evento_int
+	MOV  R1, 1			; assinala que houve uma interrup��o 0
+	MOV  [R0+6], R1		; na componente 3 da vari�vel evento_int
+						; Usa-se 6 porque cada word tem 2 bytes
+	POP  R1
+	POP  R0
+	RFE
+
+
+; **********************************************************************
+; ATRASO - Espera algum tempo
+; Argumentos: R1 - Valor a usar no contador para o atraso
+; **********************************************************************
+atraso:
+	PUSH	R1
+	MOV		R1, ATRASO
+continua:
+	SUB		R1, 1				; decrementa o contador de 1 unidade
+	JNZ		continua				; s� sai quando o contador chegar a 0
+	POP		R1
+	RET
+
+
+; **********************************************************************
+; PROCESSOS
+; **********************************************************************
+
+PROCESS		SP_teclado
+
+teclado:
+	MOV	 	R2, TEC_LIN   					; endereço do periférico das linhas
+	MOV	 	R3, TEC_COL   					; endereço do periférico das colunas
+	MOV	 	R5, MASCARA   					; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+	MOV		R6, MAX_TECLADO
+
+loop_teclado:
+	YIELD
+
+	SHR		R6, 1
+	JZ		nenhuma							; se chegar ao fim do loop e nenhuma tecla for premida
+
+	MOVB 	[R2], R6      					; a linha a ler passa a ser a do registo R6
+	MOVB 	R0, [R3]      					; ler do periférico de entrada (colunas)
+	
+	AND		R0, R5        					; elimina bits para além dos bits 0-3
+	JNZ		fim_teclado						; se alguma tecla for premida quebra o loop
+	
+	JMP		loop_teclado
+
+nenhuma:
+	MOV		R0, -1
+	MOV		[tecla_carregada], R0
+	MOV		R6, MAX_TECLADO
+	JMP		loop_teclado
+
+fim_teclado:
+	CALL	converte_tec_hex
+
+	MOV		[tecla_carregada], R0
+	
